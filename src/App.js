72d7a65ba569199a6ab5d8d2e13ad36199a9f59c
Sercan Sever <sercan.sever16@gmail.com>
2023-07-25 09:40:58 +0300

    controlled componenet inputs

--- src/App.js
+++ src/App.js
@@ -2,38 +2,45 @@ import Header from "./Header.js";
 import Content from "./Content";
 import Footer from "./Footer";
 import { useState } from "react";
+import AddItem from "./AddItem.js";
+import SearchItem from "./SearchItem.js";
 
 function App() {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "One half bag of Cacoa",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "One half bag of Candy",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "One half bag of Vodka",
-    },
-  ]);
+  const [newItem, setNewItem] = useState("");
+  const [search, setSearch] = useState("");
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("shoppingList"))
+  );
+
+  const setAndSaveItems = (newItems) => {
+    setItems(newItems);
+    localStorage.setItem("shoppingList", JSON.stringify(newItems));
+  };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setItems(listItems);
-    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-    localStorage.setItem("shoppingList", JSON.stringify(listItems));
+    setAndSaveItems(listItems);
+  };
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const newListItem = { id, checked: false, item };
+    const listItems = [...items, newListItem];
+    setAndSaveItems(listItems);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    addItem(newItem);
+    setNewItem("");
   };
 
   return (
@@ -41,9 +48,17 @@ function App() {
       {/* Overrides Header.defaultProps */}
       <Header title="Hello World!" />
       <Content
-        items={items}
+        items={items
+          .filter((item) => item.item.toLowerCase())
+          .includes(search.toLowerCase())}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
+      />
+      <SearchItem search={search} setSearch={setSearch} />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
       />
       <Footer length={items.length} />
     </div>
